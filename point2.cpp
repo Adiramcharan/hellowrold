40b4a1eb12de5276a88a08dd86e4162769de1b98
Adi Ram Charan Gurijala <adiramcharangurijala@Adis-MacBook-Air.local>
2025-05-28 19:29:35 +0530

    add point2 point2.cpp

---
+++ point2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string food="pizza";
+    string* ptr=&food;
+    cout<<ptr<<"\n";
+    cout<<*ptr<<"\n";
+    return 0;
+}
