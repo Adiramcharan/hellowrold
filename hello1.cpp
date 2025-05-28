35bc3161ab1168fa67857cdd2af963b668517cdb
Adi Ram Charan Gurijala <adiramcharangurijala@Adis-MacBook-Air.local>
2025-05-28 09:57:54 +0530

    Add new game code file

--- hello1.cpp
+++ hello1.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
-int main() {
-    std::cout<<"hello world!";
+using namespace std;
+int main(){
+    bool ships[4][4]={
+        {0,1,1,0},
+        {0,0,0,0},
+        {0,0,1,0},
+        {0,0,1,0}
+    };
+    int hits=0;
+    int numberofturns=0;
+    while (hits<4)
+    {
+        /* code */
+        int row,column;
+        cout<<"selecting coordinates\n";
+        cout<<"choose a row number between 0 and 3:";
+        cin>>row;
+        cout<<"choose a column number between 0 and 3:";
+        cin>>column;
+        if(ships[row][column]){
+            ships[row][column]=0;
+            hits++;
+            cout<<"Hit!"<<(4-hits)<<"left.\n\n";
+
+        }else{
+            cout<<"Miss\n\n";
+        }
+        numberofturns++;
+    }
+    cout<<"victory!\n";
+    cout<<"you won in "<< numberofturns<<" turns";
     return 0;
 }
