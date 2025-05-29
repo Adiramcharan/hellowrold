a7d083a587bcfd791f28ec7245464ecb21b9a729
Adi Ram Charan Gurijala <adiramcharangurijala@Adis-MacBook-Air.local>
2025-05-29 14:08:48 +0530

    add function parameter in c++

---
+++ funcpa.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+void myfunction(string fname){
+    cout<<fname<<" resfsnes\n";
+}
+int main(){
+    myfunction("liam");
+    myfunction("jenny");
+    myfunction("anja");
+    return 0;
+}
