73ea3ebc0a8178b112cd36d8c5a03e1771411324
Adi Ram Charan Gurijala <adiramcharangurijala@Adis-MacBook-Air.local>
2025-06-02 22:13:34 +0530

    multilevelinheritance in c++

---
+++ multilevelinher.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class myclass{
+    public:
+    void myfunction(){
+        cout<<"some content in parent class. ";
+    }
+};
+class mychild:public myclass{
+
+};
+class mygrandchild:public mychild{
+
+};
+int main(){
+    mygrandchild myobj;
+    myobj.myfunction();
+    return 0;
+}
+
