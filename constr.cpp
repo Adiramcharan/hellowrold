c2803a1e14c20be3b59e3e82050351da0ac45fda
Adi Ram Charan Gurijala <adiramcharangurijala@Adis-MacBook-Air.local>
2025-06-02 16:43:11 +0530

    constructors in c++

---
+++ constr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class myclass{
+    public:
+    myclass(){
+        cout<<"hello world!";
+    }
+};
+int main(){
+    myclass obj;
+    return 0;
+}
