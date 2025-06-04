2a7409be83243b1bfa542e4fce8d2487629358f8
Adi Ram Charan Gurijala <adiramcharangurijala@Adis-MacBook-Air.local>
2025-06-04 12:53:45 +0530

    reading files in c++

---
+++ readfile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(){
+    string mytext;
+    ifstream myreadfile("filename.txt");
+    while(getline(myreadfile,mytext)){
+        cout<<mytext;
+    }
+    myreadfile.close();
+    return 0;
+}
