be711307cc9e2babfcdf3f4151a0489e12d031b3
Adi Ram Charan Gurijala <adiramcharangurijala@Adis-MacBook-Air.local>
2025-06-06 18:16:31 +0530

    accessing a vector in c++

--- vectoraccess.cpp
+++ vectoraccess.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 int main(){
     vector <string> cars={"volvo","bmw","ford"};
-    cout<<cars[0];
-    cout<<cars.at(2);
+    cout<<cars[0]<<endl;
+    cout<<cars.at(2)<<endl;
+    cars.push_back("tesla");
+    cars.push_back("tata");
+    cars.push_back("byd");
+    cars.pop_back();
+    cout<<cars.size()<<endl;
+    cout<<cars.empty()<<endl;
+    cout<<cars.front()<<endl;
+    cout<<cars.back()<<endl;
+    return 0;
 }
